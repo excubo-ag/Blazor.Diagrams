66065bac5bb0060d76fc75331cfe46e49db7df00
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-06-15 17:06:57 +0200

    added service extension

---
+++ Diagram/ServiceExtension.cs
@@ -0,0 +1,13 @@
+﻿using Excubo.Blazor.LazyStyleSheet;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Excubo.Blazor.Diagrams
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddDiagramServices(this IServiceCollection services)
+        {
+            return services.AddStyleSheetLazyLoading();
+        }
+    }
+}
