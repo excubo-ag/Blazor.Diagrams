2b32f94e8155a581f23f823d6ec51e3fe32383c9
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-07-13 18:07:42 +0200

    implemented grid-lines feature (#8)

--- Diagram/Diagram.Parameters.razor.cs
+++ Diagram/Diagram.Parameters.razor.cs
@@ -13,5 +13,13 @@ namespace Excubo.Blazor.Diagrams
         /// Callback that is executed whenever the selection of nodes and/or links changes.
         /// </summary>
         [Parameter] public Action<Group> SelectionChanged { get; set; }
+        /// <summary>
+        /// Whether to show grid lines on the diagram
+        /// </summary>
+        [Parameter] public bool ShowGridLines { get; set; }
+        /// <summary>
+        /// The distance between two adjacent grid lines (only shown when ShowGridLines is set to true). Default value is 100.
+        /// </summary>
+        [Parameter] public int GridLineDistance { get; set; } = 100;
     }
 }
