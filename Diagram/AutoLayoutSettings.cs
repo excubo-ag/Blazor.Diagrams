af50e1258df49a399d04557e747b65bb45bf2887
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-09-11 11:52:47 +0200

    center nodes by widest layer

--- Diagram/AutoLayoutSettings.cs
+++ Diagram/AutoLayoutSettings.cs
@@ -165,9 +165,14 @@ namespace Excubo.Blazor.Diagrams
         private static void ArrangeNodesHorizontally(List<List<NodeBase>> layers, double vertical_separation, double horizontal_separation)
         {
             double x = 0;
-            foreach (var layer in layers)
+            var heights = layers.Select(layer =>
+                layer.Select(n => n.Height + n.GetDrawingMargins().Top + n.GetDrawingMargins().Bottom).ToList())
+                .ToList();
+            var layer_heights = heights.Select(layer => horizontal_separation * (layer.Count - 1) + layer.Sum()).ToList();
+            var highest_layer_height = layer_heights.Max();
+            foreach (var (layer, height) in layers.Zip(layer_heights, (l, h) => (l, h)))
             {
-                double y = 0;
+                double y = (highest_layer_height - height) / 2;
                 foreach (var node in layer)
                 {
                     node.MoveTo(x, y);
@@ -181,9 +186,14 @@ namespace Excubo.Blazor.Diagrams
         private static void ArrangeNodesVertically(List<List<NodeBase>> layers, double vertical_separation, double horizontal_separation)
         {
             double y = 0;
-            foreach (var layer in layers)
+            var widths = layers.Select(layer =>
+                layer.Select(n => n.Width + n.GetDrawingMargins().Left + n.GetDrawingMargins().Right).ToList())
+                .ToList();
+            var layer_widths = widths.Select(layer => horizontal_separation * (layer.Count - 1) + layer.Sum()).ToList();
+            var widest_layer_width = layer_widths.Max();
+            foreach (var (layer, width) in layers.Zip(layer_widths, (l, w) => (l, w)))
             {
-                double x = 0;
+                double x = (widest_layer_width - width) / 2;
                 foreach (var node in layer)
                 {
                     node.MoveTo(x, y);
