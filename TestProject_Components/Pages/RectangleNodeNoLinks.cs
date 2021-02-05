1bbdf1de78b488f63d4a1889f656490ffb652327
Stefan Lörwald <stefan.loerwald@gmail.com>
2021-02-05 14:21:24 +0100

    add example of unlinkable node for #79

---
+++ TestProject_Components/Pages/RectangleNodeNoLinks.cs
@@ -0,0 +1,10 @@
+﻿using Excubo.Blazor.Diagrams;
+using Microsoft.AspNetCore.Components;
+
+namespace TestProject_Components
+{
+    public class RectangleNodeNoLinks : RectangleNode
+    {
+        public override RenderFragment border => builder => { };
+    }
+}
